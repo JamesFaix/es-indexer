8881ad8daa8db09b344f3e703395292c88dd3a98
James Faix <jamesthefaix@gmail.com>
2019-09-19 10:15:25 -0400

    Add project files.

---
+++ es-indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace es_indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
