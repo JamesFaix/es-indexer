a93573169dd5e8035185d77491e49e8e621f3afc
James Faix <jamesthefaix@gmail.com>
2019-09-26 14:42:20 -0400

    Created new project

---
+++ multiple-indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multiple_indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
